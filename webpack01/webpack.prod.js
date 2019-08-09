edd1c96ebd5ced2ad47b733fdfc395b994ec7a92
liyongjie <liyongjie@tvflnet.com>
2019-08-09 17:48:28 +0800

    【 多页面应用打包的通用方案，利用glob.sync，动态获取相应入口文件 】

--- webpack01/webpack.prod.js
+++ webpack01/webpack.prod.js
@@ -3,13 +3,48 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const glob = require('glob')
+
+// 动态获取多entry和htmlWebpackPlugins
+const setMPA = () => {
+    const entry = {}
+    const htmlWebpackPlugins = []
+
+    const entryFiles = glob.sync(path.join(__dirname, './src/pages/*/index.js'))
+
+    Object
+        .keys(entryFiles)
+        .map(index => {
+            const entryFile = entryFiles[index]
+            const match = entryFile.match(/src\/pages\/(.*)\/index.js/)
+            const pageName = match && match[1]
+
+            entry[pageName] = entryFile
+            htmlWebpackPlugins.push(new HtmlWebpackPlugin({
+                template: path.join(__dirname, `src/pages/${pageName}/index.html`),
+                filename: `${pageName}.html`,
+                chunks: [pageName],
+                inject: true,
+                minify: {
+                    html5: true,
+                    collapseWhitespace: true,
+                    preserveLineBreaks: false,
+                    minifyCSS: true,
+                    minifyJS: true,
+                    removeComments: false
+                }
+            }))
+        })
+
+    return { entry, htmlWebpackPlugins }
+}
+
+const { entry, htmlWebpackPlugins } = setMPA()
+
 
 module.exports = {
     mode: 'production',
-    entry: {
-        index: './src/pages/index/index.js',
-        search: './src/pages/search/search.js'
-    },
+    entry,
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: '[name]_[chunkhash:8].js'
@@ -101,35 +136,7 @@ module.exports = {
             assetNameRegExp: /\.css$/g,
             cssProcessor: require('cssnano')
         }),
-        new HtmlWebpackPlugin({
-            template: path.join(__dirname, 'src/pages/index/index.html'),
-            filename: 'index.html',
-            chunks: ['index'],
-            inject: true,
-            minify: {
-                html5: true,
-                collapseWhitespace: true,
-                preserveLineBreaks: false,
-                minifyCSS: true,
-                minifyJS: true,
-                removeComments: false
-            }
-        }),
-        new HtmlWebpackPlugin({
-            template: path.join(__dirname, 'src/pages/search/search.html'),
-            filename: 'search.html',
-            // 入口
-            chunks: ['search'],
-            inject: true,
-            minify: {
-                html5: true,
-                collapseWhitespace: true,
-                preserveLineBreaks: false,
-                minifyCSS: true,
-                minifyJS: true,
-                removeComments: false
-            }
-        })
-
+        // 动态html
+        ...htmlWebpackPlugins
     ]
 }
