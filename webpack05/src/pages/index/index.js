1df56dc5d0e19ae536ed0ade8ca4bd2fb103711c
liyongjie <liyongjie@tvflnet.com>
2019-08-12 17:26:13 +0800

    【  动态加载cnd第三方库资源，以echarts为例  】

--- webpack05/src/pages/index/index.js
+++ webpack05/src/pages/index/index.js
@@ -4,7 +4,7 @@ import '../../../styles/reset/reset.css'
 import './index.less'
 import img1 from '../../assert/img/微信图片_20190627183624.jpg';
 import img2 from '../../assert/img/if_Halloween-06_355959.png'
-
+// import echarts from 'echarts'
 
 
 class Home extends React.Component {
@@ -15,14 +15,56 @@ class Home extends React.Component {
         let { default: fn } = await import('./d_import.js')
         console.log(fn())
     }
+    loadEcharts = async () => {
+        if (!window['echarts']) {
+            // 没加载就加载
+            await (new Promise((resolve, reject) => {
+                let script = document.createElement('script')
+                script.type = "text/javascript"
+                script.src = "https://cdn.bootcss.com/echarts/4.2.1-rc1/echarts.min.js"
+                script.onload = () => {
+                    resolve()
+                }
+                document.querySelector('body').appendChild(script)
+            }))
+        }
+        let { default: echarts } = await import('echarts')
+
+        // 基于准备好的dom，初始化echarts实例
+        var myChart = echarts.init(document.getElementById('main'));
+        // 指定图表的配置项和数据
+        var option = {
+            title: {
+                text: 'ECharts 入门示例'
+            },
+            tooltip: {},
+            legend: {
+                data: ['销量']
+            },
+            xAxis: {
+                data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
+            },
+            yAxis: {},
+            series: [{
+                name: '销量',
+                type: 'bar',
+                data: [5, 20, 36, 10, 10, 20]
+            }]
+        };
+
+        // 使用刚指定的配置项和数据显示图表。
+        myChart.setOption(option);
+    }
     render() {
         return (
             <div>
-                <span>财务得福后果额</span>
+                <span>财务得福后果额1</span>
                 <div className="h">Home Page</div>
                 <img className="img1" src={img1} />
                 <i className="web-font">英雄难过美人关，我不是英雄，美人让我过了关</i>
                 <img className="img1" src={img2} onClick={this.dynamicImport} />
+                <button onClick={this.loadEcharts}>load echarts</button>
+                <div id="main" style={{ width: '600px', height: '400px' }}></div>
             </div>
         )
 
--- webpack05/webpack.prod.js
+++ webpack05/webpack.prod.js
@@ -4,6 +4,7 @@ const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const glob = require('glob')
+const HtmlWebpackExternalsPlugin = require('html-webpack-externals-plugin')
 
 // 动态获取多entry和htmlWebpackPlugins
 const setMPA = () => {
@@ -135,6 +136,24 @@ module.exports = {
         new OptimizeCssAssetsPlugin({
             assetNameRegExp: /\.css$/g,
             cssProcessor: require('cssnano')
+        }),
+        new HtmlWebpackExternalsPlugin({
+            externals: [
+                {
+                    module: 'react',
+                    entry: 'https://11.url.cn/now/lib/16.2.0/react.min.js',
+                    global: 'React',
+                },
+                {
+                    module: 'react-dom',
+                    entry: 'https://11.url.cn/now/lib/16.2.0/react-dom.min.js',
+                    global: 'ReactDOM',
+                },{
+                    module: 'echarts',
+                    entry:'https://cdn.bootcss.com/echarts/4.2.1-rc1/echarts.min.js',
+                    global: 'echarts'
+                }
+            ],
         }),
         // 动态html
         ...htmlWebpackPlugins
