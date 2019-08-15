242126f0bc359dbda81ae8161f7926aef7253508
liyongjie <liyongjie@tvflnet.com>
2019-08-15 15:42:08 +0800

    【  assert断言测试  】

--- builder-webpack/test/unit/webpack-base-test.js
+++ builder-webpack/test/unit/webpack-base-test.js
@@ -1,9 +1,12 @@
+const assert = require('assert');
+
 describe('webpack.base.js test case', () => {
 
     const baseConfig = require('../../lib/webpack.base.js');
 
     console.log(baseConfig);
     it('entry', () => {
-
+        assert.equal(baseConfig.entry.index, 'D:/testProjects/webpack/webpack4-practice/builder-webpack/test/smoke/template/src/pages/index/index.js')
+        assert.equal(baseConfig.entry.search, 'D:/testProjects/webpack/webpack4-practice/builder-webpack/test/smoke/template/src/pages/search/index.js')
     })
 })
